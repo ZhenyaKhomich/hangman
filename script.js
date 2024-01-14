04282d7bd3e6cf8e05d65fa254e55951ee012476
Zhenya Khomich <zhenyatsoy21061992@gmail.com>
2024-01-14 20:17:53 +0400

    fix: add work score

--- script.js
+++ script.js
@@ -36,24 +36,47 @@ const listQuestions = [
         answer:"html"
     },
     {
-        question: "Is the img attribute required?",
-        answer:"alt"
+        question: "Yellow fruit that grows in Africa?",
+        answer:"banana"
     },
+    {
+        question: "A two-wheeler with pedals?",
+        answer:"bicycle"
+    }
 ];
-
+let currentAnswer;
+let startScore = 0;
+const maxScore = 6;
 const keybordBlock = document.querySelector(".keybord");
 const wordNone = document.querySelector(".word__none-letters");
+const scoreNum = document.querySelector(".score-num");
+const letterClick = (button, letterClicked) => {
+    if(currentAnswer.includes(letterClicked)) {
+        [...currentAnswer].forEach((letter, index) => {
+            if(letter === letterClicked) {
+                wordNone.querySelectorAll("li")[index].innerText = letter;
+                wordNone.querySelectorAll("li")[index].classList.add("word__none-letter");
+            }
+        });
+    } else {
+        startScore++;
+    }
+    scoreNum.innerText = `${startScore} / ${maxScore}`;
+}
+
 for(let i = 97; i <= 122; i++) {
     const button = document.createElement("button");
     button.innerText = String.fromCharCode(i);
     keybordBlock.appendChild(button);
+    button.addEventListener("click", el => letterClick(el.target, String.fromCharCode(i)));
 }
 
 const addRandonQuestion = () => {
     const {answer, question} = listQuestions[Math.floor(Math.random() * listQuestions.length)];
+    currentAnswer = answer;
     console.log(answer);
     document.querySelector(".question__text").innerText = question;
-    wordNone.innerHTML = answer.split("").map(() => `<li class="letter"></li>`).join("")
+    wordNone.innerHTML = answer.split("").map(() => `<li class="letter"></li>`).join("");
 }
 addRandonQuestion();
 
